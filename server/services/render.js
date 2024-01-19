ea2a78d25821ae7c9f1841f5351c72960973ee04
avi-kwok <146047666+avi-kwok@users.noreply.github.com>
2024-01-19 12:27:47 -0800

    Fixed hardcode render.js

--- server/services/render.js
+++ server/services/render.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const homeRoutes = async (req, res) => {
     try {
-        const response = await axios.get('http://localhost:3000/api/users');
+        const response = await axios.get('https://ultideploy.onrender.com/api/users');
         res.render('index', { users: response.data });
     } catch (err) {
         res.send(err);
@@ -17,7 +17,7 @@ export const add_user = (req, res) => {
 
 export const update_user = async (req, res) => {
     try {
-        const userdata = await axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } });
+        const userdata = await axios.get('https://ultideploy.onrender.com/api/users', { params: { id: req.query.id } });
         res.render("update_user", { user: userdata.data });
     } catch (err) {
         res.send(err);
